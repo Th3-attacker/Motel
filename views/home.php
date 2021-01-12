a4c577f3b673fed0984300fbdfabb09d2e509eea
Th3-attacker <haje.malik5@gmail.com>
2021-01-12 15:53:01 +0000

    add Rooms

--- views/home.php
+++ views/home.php
@@ -1,5 +1,5 @@
 <?php require_once VIEWS . '/partials/header.php'?>
-<div class="container">
+<div class="container mt-5 mb-5">
     <div class="row">
         <div class="col-12">
             <h2 class="text-center">Center aligned text on all viewport sizes.</h2>
---
+++ views/room.php
@@ -0,0 +1,183 @@
+<?php require_once VIEWS . '/partials/header.php'?>
+<div class="container mt-3 mb-3">
+  <div class="row">
+    <div class="col-md-4">
+      <form method="POST" action="">
+        <input type="hidden"  value="15" />
+        <input type="hidden" value="12" />
+        <div class="card">
+          <img class="card-img-top" src="./public/images/1.jpg" alt="Room image description"/>
+          <div class="card-body">
+            <div class="rooms_title"><h2>Wing A Standard Room</h2></div>
+            <div class="rooms_text">
+              <p>Without TV</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="" />
+                  <span>Number of Person - 2</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="" />
+                    <span>Remaining Rooms :1</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">15/<span>Night</span></div>
+            <div class="form-group">
+              <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                  <input type="submit" class="button rooms_button" id="booknow" name="booknow" onclick="return validateBook();" value="Book Now!"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="col-md-4">
+      <form method="POST" action="">
+        <input type="hidden"  value="15" />
+        <input type="hidden" value="12" />
+        <div class="card">
+          <img class="card-img-top" src="./public/images/2.jpg" alt="Room image description"/>
+          <div class="card-body">
+            <div class="rooms_title"><h2>Wing A Standard Room</h2></div>
+            <div class="rooms_text">
+              <p>Without TV</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="" />
+                  <span>Number of Person - 2</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="" />
+                    <span>Remaining Rooms :1</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">15/<span>Night</span></div>
+            <div class="form-group">
+              <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                  <input type="submit" class="button rooms_button" id="booknow" name="booknow" onclick="return validateBook();" value="Book Now!"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="col-md-4">
+      <form method="POST" action="">
+        <input type="hidden"  value="15" />
+        <input type="hidden" value="12" />
+        <div class="card">
+          <img class="card-img-top" src="./public/images/3.jpg" alt="Room image description"/>
+          <div class="card-body">
+            <div class="rooms_title"><h2>Wing A Standard Room</h2></div>
+            <div class="rooms_text">
+              <p>Without TV</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="" />
+                  <span>Number of Person - 2</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="" />
+                    <span>Remaining Rooms :1</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">15/<span>Night</span></div>
+            <div class="form-group">
+              <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                  <input type="submit" class="button rooms_button" id="booknow" name="booknow" onclick="return validateBook();" value="Book Now!"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+  <div class="row mt-5">
+    <div class="col-md-4">
+      <form method="POST" action="">
+        <input type="hidden"  value="15" />
+        <input type="hidden" value="12" />
+        <div class="card">
+          <img class="card-img-top" src="./public/images/4.jpg" alt="Room image description"/>
+          <div class="card-body">
+            <div class="rooms_title"><h2>Wing A Standard Room</h2></div>
+            <div class="rooms_text">
+              <p>Without TV</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="" />
+                  <span>Number of Person - 2</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="" />
+                    <span>Remaining Rooms :1</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">15/<span>Night</span></div>
+            <div class="form-group">
+              <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                  <input type="submit" class="button rooms_button" id="booknow" name="booknow" onclick="return validateBook();" value="Book Now!"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="col-md-4">
+      <form method="POST" action="">
+        <input type="hidden"  value="15" />
+        <input type="hidden" value="12" />
+        <div class="card">
+          <img class="card-img-top" src="./public/images/5.jpg" alt="Room image description"/>
+          <div class="card-body">
+            <div class="rooms_title"><h2>Wing A Standard Room</h2></div>
+            <div class="rooms_text">
+              <p>Without TV</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="" />
+                  <span>Number of Person - 2</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="" />
+                    <span>Remaining Rooms :1</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">15/<span>Night</span></div>
+            <div class="form-group">
+              <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                  <input type="submit" class="button rooms_button" id="booknow" name="booknow" onclick="return validateBook();" value="Book Now!"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<?php require_once VIEWS . '/partials/footer.php'?>
