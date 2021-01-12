7fa364c6cc57605d462336afd307d24de41e45fe
MamcoSy <mamco.aly.sy@gmail.com>
2021-01-12 14:11:31 +0000

    Add routes to a separate file

--- index.php
+++ index.php
@@ -5,28 +5,7 @@ define('VIEWS', ROOT . DS . 'views');
 
 $path = $_GET['page'] ?? 'home';
 
-$map = [
-	'home'       => [
-		'_view' => VIEWS . DS . 'home.php'
-		// '_controller' => 'HomeController@inddex'
-	],
-	'contact-us' => [
-		'_view' => VIEWS . DS . 'contact-us.php'
-		// '_controller' => 'ContactController'
-	],
-	'about-us'   => [
-		'_view' => VIEWS . DS . 'about-us.php'
-		// '_controller' => 'AboutController'
-	],
-	'room'       => [
-		'_view' => VIEWS . DS . 'room.php'
-		// '_controller' => 'RoomController'
-	],
-	'404'        => [
-		'_view' => VIEWS . DS . '404.php'
-		// '_controller' => 'RoomController'
-	]
-];
+$map = require_once ROOT . DS . 'routes.php';
 
 $defaultView       = $map['404']['_view'];
 $defaultStatusCode = 404;
---
+++ routes.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+	'home'       => [
+		'_view' => VIEWS . DS . 'home.php'
+		// '_controller' => 'HomeController@inddex'
+	],
+	'contact-us' => [
+		'_view' => VIEWS . DS . 'contact-us.php'
+		// '_controller' => 'ContactController'
+	],
+	'about-us'   => [
+		'_view' => VIEWS . DS . 'about-us.php'
+		// '_controller' => 'AboutController'
+	],
+	'room'       => [
+		'_view' => VIEWS . DS . 'room.php'
+		// '_controller' => 'RoomController'
+	],
+	'404'        => [
+		'_view' => VIEWS . DS . '404.php'
+		// '_controller' => 'RoomController'
+	]
+];
