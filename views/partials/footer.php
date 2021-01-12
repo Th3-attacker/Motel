ed4ed2cde592a2ab53dad5d0b892548e9896f753
MamcoSy <mamco.aly.sy@gmail.com>
2021-01-12 18:36:14 +0000

    Fixing footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-    <div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="/marimar/images/footer.jpg" data-speed="0.8"></div>
+    <div class="parallax_background" data-parallax="scroll" data-image-src="/marimar/images/footer.jpg" data-speed="0.8"></div>
     <div class="container">
         <div class="row">
             <div class="col">
@@ -36,18 +36,17 @@
                     Copyright &copy;<script>
                     document.write(new Date().getFullYear());
                     </script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 </div>
             </div>
         </div>
     </div>
 </footer>
-</body>
 <script src="./public/js/script.js"></script>
 <script src="./public/js/bootstrap.js"></script>
 <script src="./public/js/all.js"></script>
 <script src="./public/js/all.min.js"></script>
 <script src="./public/js/bootstrap.min.js"></script>
 <script src="./public/js/owl-carousel.js"></script>
+</body>
 
 </html>
