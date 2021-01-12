69514d668cf999708046da94e68e68288b476956
MamcoSy <mamco.aly.sy@gmail.com>
2021-01-12 14:08:24 +0000

    Refactorisation

--- index.php
+++ index.php
@@ -6,25 +6,36 @@ define('VIEWS', ROOT . DS . 'views');
 $path = $_GET['page'] ?? 'home';
 
 $map = [
-    'home' => [
-        '_view' => VIEWS . DS . 'home.php',
-        // '_controller' => 'HomeController'
-    ],
-    'contact-us' => [
-        '_view' => VIEWS . DS . 'contact-us.php',
-        // '_controller' => 'ContactController'
-    ],
-    'about-us' => [
-        '_view' => VIEWS . DS . 'about-us.php',
-        // '_controller' => 'AboutController'
-    ],
-    'room' => [
-        '_view' => VIEWS . DS . 'room.php',
-        // '_controller' => 'RoomController'
-    ],
+	'home'       => [
+		'_view' => VIEWS . DS . 'home.php'
+		// '_controller' => 'HomeController@inddex'
+	],
+	'contact-us' => [
+		'_view' => VIEWS . DS . 'contact-us.php'
+		// '_controller' => 'ContactController'
+	],
+	'about-us'   => [
+		'_view' => VIEWS . DS . 'about-us.php'
+		// '_controller' => 'AboutController'
+	],
+	'room'       => [
+		'_view' => VIEWS . DS . 'room.php'
+		// '_controller' => 'RoomController'
+	],
+	'404'        => [
+		'_view' => VIEWS . DS . '404.php'
+		// '_controller' => 'RoomController'
+	]
 ];
+
+$defaultView       = $map['404']['_view'];
+$defaultStatusCode = 404;
+
 if (isset($map[$path])) {
-    require_once $map[$path]['_view'];
-} else {
-    echo 'Page introuvable';
+	$defaultView       = $map[$path]['_view'];
+	$defaultStatusCode = 200;
 }
+
+http_response_code($defaultStatusCode);
+header('Content-type: text/html; charset=utf-8');
+require_once $defaultView;
---
+++ views/404.php
@@ -0,0 +1,5 @@
+<?php require_once VIEWS . '/partials/header.php'?>
+<div style="text-align: center;margin: 15vh">
+    <h1>Error 404</h1>
+    <p>La page que vous essaiyé de charger n'existe pas.</p>
+</div>
